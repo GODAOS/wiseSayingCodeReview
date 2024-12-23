99c3971a9271ae2ecbcca8ed70cc6f1bdb4a3ff2
GODAOS <jmskkr@gmail.com>
2024-12-23 15:08:27 +0900

    work2

---
+++ src/main/java/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public void run() {
+        System.out.println("== 명언 앱 ==");
+        System.out.println("명언 : 현재를 사랑하라.");
+        System.out.println("작가 : 작가미상");
+        System.out.println("명령) 종료");
+    }
+}
