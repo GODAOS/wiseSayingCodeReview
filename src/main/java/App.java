7c7d19f754b921fd4db437db62d47394ed3ae8b7
GODAOS <jmskkr@gmail.com>
2024-12-23 15:20:33 +0900

    work3

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,8 +1,17 @@
+import java.util.Scanner;
+
 public class App {
     public void run() {
+        String wise;
+        String author;
+
+        Scanner sc = new Scanner(System.in);
         System.out.println("== 명언 앱 ==");
-        System.out.println("명언 : 현재를 사랑하라.");
-        System.out.println("작가 : 작가미상");
+        System.out.print("명언 : ");
+        wise = sc.nextLine();// 입력값 가져온다. enter로 마무리
+        System.out.print("작가 : ");
+        author = sc.nextLine();
+        System.out.println("1번 명언이 등록되었습니다.");
         System.out.println("명령) 종료");
     }
 }
