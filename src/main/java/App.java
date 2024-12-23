ec19f134690f87b32056d569fbe72d352ab2651c
GODAOS <jmskkr@gmail.com>
2024-12-23 17:02:33 +0900

    work5

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,26 +1,35 @@
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class App {
     public void run() {
         String wise;
         String author;
+        HashMap<Integer,String> wise_list = new HashMap<>();
         int cnt = 0;
 
         Scanner sc = new Scanner(System.in);
         System.out.println("== 명언 앱 ==");
         while (true){
-            cnt++;
             System.out.print("명령) ");
-            String registration = sc.nextLine();
+            String command = sc.nextLine();
 
-            if (registration.equals("등록")){
+            if (command.equals("종료")) {
+                break;
+            } else if (command.equals("등록")){
                 System.out.print("명언 : ");
                 wise = sc.nextLine();// 입력값 가져온다. enter로 마무리
                 System.out.print("작가 : ");
                 author = sc.nextLine();
+                cnt++;
+                wise_list.put(cnt,author+" / "+wise);
                 System.out.println(cnt+"번 명언이 등록되었습니다.");
-            } else if (registration.equals("종료")) {
-                break;
+            } else if (command.equals("목록")) {
+                System.out.println("번호 / 작가 / 명언");
+                System.out.println("----------------------");
+                for (int i = wise_list.size(); i > 0; i--) {
+                    System.out.println(i+" / "+wise_list.get(i));
+                }
             }
         }
     }
