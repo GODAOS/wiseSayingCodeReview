c98c585086603ae15dadc015dd3f0ef9327306ca
GODAOS <jmskkr@gmail.com>
2024-12-23 15:34:50 +0900

    work4

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,14 +4,24 @@ public class App {
     public void run() {
         String wise;
         String author;
+        int cnt = 0;
 
         Scanner sc = new Scanner(System.in);
         System.out.println("== 명언 앱 ==");
-        System.out.print("명언 : ");
-        wise = sc.nextLine();// 입력값 가져온다. enter로 마무리
-        System.out.print("작가 : ");
-        author = sc.nextLine();
-        System.out.println("1번 명언이 등록되었습니다.");
-        System.out.println("명령) 종료");
+        while (true){
+            cnt++;
+            System.out.print("명령) ");
+            String registration = sc.nextLine();
+
+            if (registration.equals("등록")){
+                System.out.print("명언 : ");
+                wise = sc.nextLine();// 입력값 가져온다. enter로 마무리
+                System.out.print("작가 : ");
+                author = sc.nextLine();
+                System.out.println(cnt+"번 명언이 등록되었습니다.");
+            } else if (registration.equals("종료")) {
+                break;
+            }
+        }
     }
 }
